d0d6485f3bb7bb38b736c92072166051559a042a
KokoszDellProData <tomasz.kostyrka@gmail.com>
2020-06-04 23:54:01 +0200

    before 4/Bytes

---
+++ Tutorial01/Basics02.py
@@ -0,0 +1,16 @@
+
+print("first -> " "second")
+
+q1 = "nowa\nlinia\nna ekranie\t\t\ti kilka tabow"
+q2 = r"nowa\nlinia\nna ekranie\t\t\ti kilka tabow"
+
+print(q1)
+print(q2)
+
+s = 'kokosz'
+i = 1
+
+while i <= len(s):
+    print("literka: " + (s[i]).upper())
+    i += 1
+
