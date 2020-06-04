0b3843161a25d9347f744d9bdd59c0e06ec1667c
KokoszDellProData <tomasz.kostyrka@gmail.com>
2020-06-04 23:28:28 +0200

    basics 01

--- Tutorial01/Basics01.py
+++ Tutorial01/Basics01.py
@@ -1,1 +1,19 @@
 print("hello!")
+
+if True:
+    print("dummy test!")
+
+if not(False):
+    print("dummy test#2 line #1!")
+    print("dummy test#2 line #2!")
+
+i=1
+while i<=10:
+   print("iteration: " + str(i))
+   i += 1
+
+while True:
+    response = input("provide int:")
+    if int(response) % 7 == 0:
+        break
+
