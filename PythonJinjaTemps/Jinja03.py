761f0e15b9286facf6d4526a5aba93a1cc9de35e
KokoszDellProData <tomasz.kostyrka@gmail.com>
2020-06-24 12:43:27 +0200

    jinja basics

---
+++ PythonJinjaTemps/Jinja03.py
@@ -0,0 +1,9 @@
+from jinja2 import Template
+from classes.Person import *
+
+person = Person('Peter', 34)
+
+tm = Template("My name is {{ per.get_name() }} and I am {{ per.get_age() }}")
+msg = tm.render(per=person)
+
+print(msg)
---
+++ PythonJinjaTemps/classes/Person.py
@@ -0,0 +1,12 @@
+class Person:
+
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
